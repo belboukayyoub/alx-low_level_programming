2d239be8314010b822758a6bb94f3155ceb17b03
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-03-23 15:04:09 -0700

    Task - 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: input of function
+ *
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef _ISUPPER_H
+#define _ISUPPER_H
+
+int _isupper(int c);
+
+#endif
