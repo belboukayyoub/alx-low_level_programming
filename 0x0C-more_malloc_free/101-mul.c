d8229d457994586dc6d1062b4631d413792f19db
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-04-13 19:59:50 -0700

    Task - 5. We must accept finite disappointment, but never lose infinite hope

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main -  multiplies two positive numbers.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+    	(void)argv;
+	return (0);
+}
