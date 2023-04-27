d400fe19bde7bc84cddb9823efc2f14d7c8c0dfb
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-04-27 13:38:13 -0700

    Fixe task - 0: Refactoring code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,16 @@
 
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
-		return (0);
-	if (h->str == NULL || h->len == 0)
-		printf("[0] (nill)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	return (1 + print_list(h->next));
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
