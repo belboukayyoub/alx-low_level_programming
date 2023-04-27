3b8261a955f1f13da60e32873bbd288efba602ec
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-04-27 13:23:49 -0700

    Fixe task - 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 {
 	if (h == NULL)
 		return (0);
-	if (h->str == NULL)
+	if (h->str == NULL || h->len == 0)
 		printf("[0] (nill)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
