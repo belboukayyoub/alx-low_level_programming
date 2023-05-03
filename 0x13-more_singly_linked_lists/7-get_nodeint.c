53f6eff02e31c1f892b77d4fd5fa6a90aeb9096c
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-05-02 18:04:27 -0700

    Fixe Task - 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head; i++)
 		head = head->next;
-		if (!head)
-			return (NULL);
 	return (head);
 
 }
