b43d4ac8a72660b8c2229bb0d371a053385902dc
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-05-02 19:21:50 -0700

    Fix Task -9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,10 +28,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->n = n;
 		*head = new;
 		return (new);
-		
 	}
 	tmp = *head;
-	for (i = 0; i < idx - 1 && tmp; i++)	
+	for (i = 0; i < idx - 1 && tmp; i++)
 		tmp = tmp->next;
 	if (!tmp)
 	{
@@ -40,5 +39,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->next = tmp->next;
 	tmp->next = new;
-	return (new);	
+	return (new);
 }
