6c49be8734fabe81da09018fdf94ab9adad29763
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-05-05 12:47:36 -0700

    Fix Task - 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,20 @@
 #include "main.h"
 #include <stddef.h>
+/**
+ * print_binary_helper - helper function.
+ *
+ * @n: unsigned long int
+ *
+ * Return: VOID.
+ */
+
+void print_binary_helper(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_binary_helper(n >> 1);
+	_putchar((n & 1) + '0');
+}
 
 /**
  * print_binary - prints the binary representation of a number.
@@ -11,8 +26,8 @@
 
 void print_binary(unsigned long int n)
 {
-	if(n == 0)
-		return;
-	print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary_helper(n);
 }
