2945fdfa8afbaa5d7021b9b99653353539b3385d
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-10-26 00:33:14 +0100

    0x1C Task 5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function that measuring island perimeter."""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island described in grid.
+
+    Args:
+        grid (list): A list of list of integers representing
+        an island while 0 represents a water zone
+        and 1 represents a land zone.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    size = 0
+    edges = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
